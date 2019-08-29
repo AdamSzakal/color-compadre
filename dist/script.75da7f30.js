09cad14f40b7d50f8638752f80520811a26c4dea
Adam Szakal <adam.b.szakal@gmail.com>
2019-08-29 22:11:18 +0200

    fix dependencies

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -58623,7 +58623,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64136" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64862" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
